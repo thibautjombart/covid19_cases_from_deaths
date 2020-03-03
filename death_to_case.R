d70277206352db219ef3f62cb35af3d5d13b8391
seabbs <s.e.abbott12@gmail.com>
2020-03-03 16:25:56 +0000

    added button as reactive event

--- death_to_case.R
+++ death_to_case.R
@@ -16,69 +16,70 @@ lapply(scripts, source)
 
 ## Define UI for application that draws a histogram
 ui <- fluidPage(
+  
+  ## Application title
+  titlePanel("COVID-19: infer cases from deaths"),
+  
+  ## Inputs for the model below
+  sidebarLayout(
+    sidebarPanel(
+      h2("Data input"),
+      textInput("date_death",
+                paste("Enter dates of death here on separate lines,",
+                      "using 'yyyy-mm-dd' format, e.g. 2020-02-18:"),
+                paste(Sys.Date(), "...")),
+      verbatimTextOutput("date_clean"),
+      h2("Model input"),
+      sliderInput("R",
+                  "Reproduction number (R):",
+                  min = 1,
+                  max = 4,
+                  value = 2,
+                  step = 0.1),
+      sliderInput("cfr",
+                  "Case Fatality Ratio (CFR), in %:",
+                  min = 0.1,
+                  max = 10,
+                  value = 2,
+                  step = 0.1),
+      sliderInput("duration",
+                  "Duration of simulations (days after last death):",
+                  min = 0,
+                  max = 14,
+                  value = 1,
+                  step = 1),
+      sliderInput("n_sim",
+                  "Number of simulation (50 trajectories per simulations):",
+                  min = 10,
+                  max = 200,
+                  value = 100,
+                  step = 10),
+      actionButton("go", "Simulate")
+    ),
     
-    ## Application title
-    titlePanel("COVID-19: infer cases from deaths"),
-    
-    ## Inputs for the model below
-    sidebarLayout(
-        sidebarPanel(
-            h2("Data input"),
-            textInput("date_death",
-                      paste("Enter dates of death here on separate lines,",
-                            "using 'yyyy-mm-dd' format, e.g. 2020-02-18:"),
-                      paste(Sys.Date(), "...")),
-            verbatimTextOutput("date_clean"),
-            h2("Model input"),
-            sliderInput("R",
-                        "Reproduction number (R):",
-                        min = 1,
-                        max = 4,
-                        value = 2,
-                        step = 0.1),
-            sliderInput("cfr",
-                        "Case Fatality Ratio (CFR), in %:",
-                        min = 0.1,
-                        max = 10,
-                        value = 2,
-                        step = 0.1),
-            sliderInput("duration",
-                        "Duration of simulations (days after last death):",
-                        min = 0,
-                        max = 14,
-                        value = 1,
-                        step = 1),
-            sliderInput("n_sim",
-                        "Number of simulation (50 trajectories per simulations):",
-                        min = 10,
-                        max = 200,
-                        value = 100,
-                        step = 10)
+    ## Show a plot of the generated distribution
+    mainPanel(
+      
+      tabsetPanel(
+        tabPanel("Explanations",
+                 includeMarkdown("include/description.md")
         ),
-        
-        ## Show a plot of the generated distribution
-        mainPanel(
-
-            tabsetPanel(
-                tabPanel("Explanations",
-                         includeMarkdown("include/description.md")
-                         ),
-                tabPanel("Graphics",
-                         ## main plot
-                         h2("Simulation results: new cases per day"),
-                         plotOutput("projections_plot"),
-                         h2("Simulation results: cumulative cases"),
-                         plotOutput("projections_plot_cumul"),
-                         ),
-                tabPanel("Summary table",
-                         h2("Summary table: cumulative case counts"),
-                         DT::dataTableOutput("summary_table", width = "60%"),
-                         br(),
-                         br()
-                         )
-            )
+        tabPanel("Graphics",
+                 ## main plot
+                 h2("Simulation results: new cases per day"),
+                 plotOutput("projections_plot"),
+                 h2("Simulation results: cumulative cases"),
+                 plotOutput("projections_plot_cumul"),
+        ),
+        tabPanel("Summary table",
+                 h2("Summary table: cumulative case counts"),
+                 DT::dataTableOutput("summary_table", width = "60%"),
+                 br(),
+                 br()
         )
+      )
     )
+  )
 )
 
 
@@ -86,7 +87,7 @@ ui <- fluidPage(
 
 ## Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  
   ## auxiliary function used to generate the table summary
   summary_function <- function(x) {
     c(q_025 = round(quantile(x, 0.025)),
@@ -94,20 +95,20 @@ server <- function(input, output) {
       q_75 = round(quantile(x, 0.75)),
       q_95 = round(quantile(x, 0.975)))
   }
-
-
+  
+  
   ## capture date of death, clean input, convert to Date
-  date_death <- reactive({
+  date_death <- eventReactive(input$go, {
     date_txt <- unlist(strsplit(input$date_death,
                                 split = "[[:blank:][:space:],;]+"))
     clean_date <- gsub("[.]", "", date_txt)
     clean_date <- as.Date(unlist(clean_date))
     clean_date[!is.na(clean_date)]
-  })
-
-
+  }, ignoreNULL = FALSE)
+  
+  
   ## simulator part
-  x <- reactive({
+  x <- eventReactive(input$go, {
     if (length(date_death())) {
       simulate_cases(date_death(),
                      R = input$R,
@@ -117,21 +118,21 @@ server <- function(input, output) {
     } else {
       NULL
     }
-  })
-
+  }, ignoreNULL = FALSE)
+  
   output$projections_plot <- renderPlot({
     if (!is.null(x)) {
       x()$plot_projections
     }
   })
-
+  
   output$projections_plot_cumul <- renderPlot({
     if (!is.null(x)) {
       x()$plot_projections_cumul
     }
   })
-
-
+  
+  
   output$summary_table <- DT::renderDataTable({
     if (!is.null(x)) {
       cumulative_counts <- cumulate(x()$projections)
